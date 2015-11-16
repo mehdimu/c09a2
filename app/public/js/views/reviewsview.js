ad257f82bc7d1c58f07cecf86c60cebb5f92ea38
arshadas <arshadas@github.com>
2015-11-16 14:58:02 -0500

    Starting step 3

---
+++ app/public/js/views/reviewsview.js
@@ -0,0 +1,42 @@
+"use strict";
+
+var splat =  splat || {};
+
+splat.ReviewsView = Backbone.View.extend({
+
+    reviewsTemplate: _.template([
+        "<% reviews.each(function(review) { %>",
+            "<%= reviewTemplate(review.toJSON()) %>",
+        "<% }); %>",
+    ].join('')),
+
+    initialize: function() {
+        // other stuff ? ...
+        // invoke showScore and renderReviews methods when collection is sync'd
+        /*showScore() checks to see if collection associated
+with this ReviewsView is empty
+– if so displays a static message (“… no reviews yet”)
+– if not, displays a freshness-score computed as
+freshVotes/freshTotal
+*/
+        this.render();
+        this.listenTo(this.reviews, "sync", this.showScore);
+        this.listenTo(this.reviews, "sync", this.renderReviews);
+    },
+
+    render: function() {
+        // render self (from ReviewsView template), then Reviewer subview,
+        // then ReviewThumbs subview, then showFreshness (current aggregate rating)
+        // can all be chained, as in this.renderX().renderY().renderZ() ...
+        console.log("called in reviews.js render");
+        var reviewThumbView = new splat.ReviewThumb();
+        var html = this.reviewsTemplate({
+            reviews: this.collection,
+            reviewTemplate: reviewThumbView.template
+            });
+        $(this.el).append(html);
+
+        return this;
+    }
+
+});
