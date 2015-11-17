f07eb3762fb0aa8c870e0e04449fada36c7307e0
arshadas <arshadas@github.com>
2015-11-17 14:24:40 -0500

    Step 3.4 done ish

--- app/app.js
+++ app/app.js
@@ -83,6 +83,8 @@ app.get('/movies/:id/reviews', splat.getReviews);
 
 app.post('/movies/:id/reviews', splat.addReview);
 
+app.get('/movies/:id/video', splat.playMovie);
+
 // ADD CODE to support other routes listed on assignment handout
 
 // location of app's static content ... may need to ADD CODE
